8fb86d766b05117fb9cb0246e2ee7cad2415c013
andraurl <36300791+andraurl@users.noreply.github.com>
2019-01-29 15:35:25 +0100

    Part 2 (#1)
    * Initial commit
    
    Added handouts
    
    * Incomplete
    
    This is where we're at after first day at lab. Some errors due to  the constraits. The QP is infeasable making the problem umpossible to solve.
    
    * Update .DS_Store
    
    * Update .DS_Store
    
    * Added files from current master

--- Task_10_2.m
+++ Task_10_2.m
@@ -3,33 +3,46 @@
 % Updated spring 2018, Andreas L. Flåten
 
 %% Initialization and model definition
-initXX; % Change this to the init file corresponding to your helicopter
+init04; % Change this to the init file corresponding to your helicopter
+addpath('Help functions')
 
+
+PI  = 3.1516;
 % Discrete time system model. x = [lambda r p p_dot]'
-delta_t	= 0.25; % sampling time
-A1 = [];
-B1 = [];
+delta_t	= 0.25;          %sampling time
+h = delta_t;             %time step
+
+A1 = [1 h 0 0 ;
+    0 1 -h*K_2 0;
+    0 0 1 h;
+    0 0 -h*K_1*K_pp 1-h*K_1*K_pd];
+B1 = [0; 0; 0; h*K_1*K_pp];
 
 % Number of states and inputs
 mx = size(A1,2); % Number of states (number of columns in A)
 mu = size(B1,2); % Number of inputs(number of columns in B)
 
 % Initial values
-x1_0 = ;                               % Lambda
-x2_0 = ;                               % r
-x3_0 = ;                               % p
-x4_0 = ;                               % p_dot
+x1_0 = PI;                          % Lambda
+x2_0 = 0;                               % r
+x3_0 = 0;                               % p
+x4_0 = 0;                               % p_dot
 x0 = [x1_0 x2_0 x3_0 x4_0]';           % Initial values
 
+%Final Values
+x_f = [0 0 0 0]';                       % Final values
+
 % Time horizon and initialization
-N  = ;                                  % Time horizon for states
+sim_time = 25; 
+N  = 1/delta_t*sim_time;                % Time horizon for states
 M  = N;                                 % Time horizon for inputs
+nx = N*length(x0);
 z  = zeros(N*mx+M*mu,1);                % Initialize z for the whole horizon
 z0 = z;                                 % Initial value for optimization
 
 % Bounds
-ul 	    = ;                   % Lower bound on control
-uu 	    = ;                   % Upper bound on control
+ul 	    = -30*PI/180;                   % Lower bound on control
+uu 	    = -ul;                           % Upper bound on control
 
 xl      = -Inf*ones(mx,1);              % Lower bound on states (no bound)
 xu      = Inf*ones(mx,1);               % Upper bound on states (no bound)
@@ -37,27 +50,37 @@ xl(3)   = ul;                           % Lower bound on state x3
 xu(3)   = uu;                           % Upper bound on state x3
 
 % Generate constraints on measurements and inputs
-[vlb,vub]       = ; % hint: gen_constraints
+[vlb,vub]       = gen_constraints(N,M,xl,xu,ul,uu); % hint: gen_constraints
 vlb(N*mx+M*mu)  = 0;                    % We want the last input to be zero
 vub(N*mx+M*mu)  = 0;                    % We want the last input to be zero
 
 % Generate the matrix Q and the vector c (objecitve function weights in the QP problem) 
 Q1 = zeros(mx,mx);
-Q1(1,1) = ;                            % Weight on state x1
-Q1(2,2) = ;                            % Weight on state x2
-Q1(3,3) = ;                            % Weight on state x3
-Q1(4,4) = ;                            % Weight on state x4
-P1 = 0;                                % Weight on input
-Q = ;                                  % Generate Q, hint: gen_q
-c = ;                                  % Generate c, this is the linear constant term in the QP
+Q1(1,1) = 2;                            % Weight on state x1
+Q1(2,2) = 0;                            % Weight on state x2
+Q1(3,3) = 0;                            % Weight on state x3
+Q1(4,4) = 0;                            % Weight on state x4
+q = 0.1; 
+P1 = q;                                 % Weight on input
+Q = gen_q(Q1,P1,N,M);                   % Generate Q, hint: gen_q
+c = gen_constraints(N,M,xl,xu,ul,uu);   % Generate c, this is the linear constant term in the QP
 
 %% Generate system matrixes for linear model
-Aeq = ;             % Generate A, hint: gen_aeq
-beq = ;             % Generate b
+Aeq = gen_aeq(A1,B1,N,mx,mu);          % Generate A, hint: gen_aeq
+beq = zeros(nx,1);                     % Generate b
+beq(1:4) = A1*x0; 
 
 %% Solve QP problem with linear model
+
+A = zeros(1,nx+M);               % inequality constraints
+B = zeros(1,1);              % inequality constraints
+
+f = zeros(N*mx + M*mu,1);
+f(1:N*mx,1) = repmat(x_f,N,1);         % = [x_f ... x_f]
+f(N*mx+1:N*mx+M,1) = repmat(q,M,1);
+
 tic
-[z,lambda] = ; % hint: quadprog. Type 'doc quadprog' for more info 
+[z,lambda] = quadprog(Q, f, A, B, Aeq, beq, ul, uu); % hint: quadprog. Type 'doc quadprog' for more info 
 t1=toc;
 
 % Calculate objective value
